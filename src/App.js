a144f61ab573109d36271c214bb60fcd480cf269
Dewaley <adewaleademilua@gmail.com>
2022-06-25 00:44:51 +0100

    Navbar implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Home from './components/Home';
+import Home from './pages/Home';
 import Nav from './components/Nav';
 
 function App() {
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { BiLogIn } from 'react-icons/bi';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { IoCloseOutline } from 'react-icons/io5';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 const Nav = () => {
   const [hamburger, setHamburger] = useState(false);
@@ -24,7 +25,7 @@ const Nav = () => {
         </ul>
         <div
           className='md:hidden rounded border-white border-2 cursor-pointer'
-          onClick={() => setHamburger(true)}
+          onClick={() => setHamburger(!hamburger)}
         >
           <button className='p-0.5 bg-white text-primary m-0.5 rounded'>
             <GiHamburgerMenu />
@@ -32,32 +33,34 @@ const Nav = () => {
         </div>
       </nav>
       <div
-        className={`absolute left-[50%] translate-x-[-50%] mx-auto w-full md:hidden ${
+        className={`absolute transition-[top] left-[50%] translate-x-[-50%] mx-auto w-full md:hidden ${
           hamburger ? 'top-20' : '-top-60'
         }`}
       >
-        <nav className='bg-primary text-center rounded mx-10 py-2 relative'>
-          <h1 className='text-xl mb-2'>
-            <span className='text-secondary'>E-</span>novate Labs
-          </h1>
-          <ul className='flex flex-col justify-center items-center gap-y-2'>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Courses</li>
-            <li>Blog</li>
-            <li>Contact</li>
-            <li className='flex items-center gap-x-1 bg-secondary p-1 rounded'>
-              <BiLogIn />
-              login
-            </li>
-          </ul>
-          <button
-            className='absolute top-2 right-10 text-2xl cursor-pointer'
-            onClick={() => setHamburger(false)}
-          >
-            <IoCloseOutline />
-          </button>
-        </nav>
+        <OutsideClickHandler onOutsideClick={() => setHamburger(false)}>
+          <nav className='bg-primary text-center rounded mx-10 py-2 relative'>
+            <h1 className='text-xl mb-2'>
+              <span className='text-secondary'>E-</span>novate Labs
+            </h1>
+            <ul className='flex flex-col justify-center items-center gap-y-2'>
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Courses</li>
+              <li>Blog</li>
+              <li>Contact</li>
+              <li className='flex items-center gap-x-1 bg-secondary p-1 rounded'>
+                <BiLogIn />
+                login
+              </li>
+            </ul>
+            <button
+              className='absolute top-2 right-10 text-2xl cursor-pointer'
+              onClick={() => setHamburger(false)}
+            >
+              <IoCloseOutline />
+            </button>
+          </nav>
+        </OutsideClickHandler>
       </div>
     </div>
   );
