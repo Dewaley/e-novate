d31f0fd3365b099d821ac8bd7b559556ea074ea8
Dewaley <adewaleademilua@gmail.com>
2022-06-29 16:03:13 +0100

    Search section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -332,17 +332,21 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className='bg-primary w-[90vw] rounded-lg flex items-center justify-center text-white flex-col mb-8'>
-        <div className='text-center'>
-          <h2 className='text-secondary my-2'>NEWSLETTER</h2>
-          <p className='text-xl mb-8'>Subscribe to our Newsletter</p>
+      <div className='bg-primary w-[90vw] rounded-lg flex items-center justify-center text-white flex-col md:flex-row md:gap-x-8 md:justify-center md:items-center mb-8 md:py-6 lg:w-[70vw]'>
+        <div className='text-center md:text-left'>
+          <h2 className='text-secondary my-2 md:my-0'>NEWSLETTER</h2>
+          <p className='text-xl mb-8 md:mb-0'>Subscribe to our Newsletter</p>
         </div>
-        <div className='w-[95%] md:w-auto bg-secondary'>
-          <form action="">
-            <input type="text" name="" id="" className='w-[85%]'/>
-            <input type="submit" value="Submit" className='float-right px-4'/>
-          </form>
-        </div>
+        <form className='bg-white py-2 px-5 md:m-0 mb-4 rounded-full'>
+          <input
+            type='text'
+            name=''
+            id=''
+            placeholder='E-mail Address'
+            className='max-w-[400px] w-[50vw] md:w-[30vw] border-none outline-none text-primary mr-0.5 placeholder:text-primary'
+          />
+          <input type='submit' value='Submit' className='p-2 bg-secondary text-white p-3 rounded-full' />
+        </form>
       </div>
     </div>
   );
