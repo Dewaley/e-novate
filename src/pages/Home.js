91b2d684bd576ef81e000a3e1cc3719ecefdda02
Dewaley <adewaleademilua@gmail.com>
2022-07-01 15:11:28 +0100

    Footer Implemented

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,14 @@ import {
   AiOutlineMessage,
   AiOutlineArrowRight,
 } from 'react-icons/ai';
-import { BsBookHalf } from 'react-icons/bs';
-import { FaChalkboardTeacher } from 'react-icons/fa';
+import { BsBookHalf, BsTelephone } from 'react-icons/bs';
+import {
+  FaChalkboardTeacher,
+  FaFacebookF,
+  FaWhatsapp,
+  FaLinkedinIn,
+  FaInstagram,
+} from 'react-icons/fa';
 import { HiOutlineDocumentDuplicate } from 'react-icons/hi';
 import { TiUser } from 'react-icons/ti';
 import { TbCertificate } from 'react-icons/tb';
@@ -13,9 +19,14 @@ import picture1 from '../images/home/picture.png';
 import picture2 from '../images/home/Online learning-cuate.png';
 import picture3 from '../images/home/pexels-burst-374074.png';
 import tinyImage from '../images/home/lessons.svg';
-import { MdOutlineNavigateNext, MdOutlineNavigateBefore } from 'react-icons/md';
+import {
+  MdOutlineNavigateNext,
+  MdOutlineNavigateBefore,
+  MdOutlineMailOutline,
+} from 'react-icons/md';
 import { ImQuotesLeft, ImQuotesRight } from 'react-icons/im';
 import { RiStarSFill } from 'react-icons/ri';
+import { IoLocationOutline } from 'react-icons/io5';
 
 const Home = () => {
   return (
@@ -188,18 +199,18 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className='absolute text-xl  flex justify-center items-center w-12 h-12 shadow-md top-[50%] right-[-1.5rem] translate-y-[-50%]  bg-white rounded-full'>
+          <div className='hidden absolute text-xl  sm:flex justify-center items-center w-12 h-12 shadow-md top-[50%] right-[-1.5rem] translate-y-[-50%]  bg-white rounded-full'>
             <MdOutlineNavigateNext />
           </div>
         </div>
       </div>
-      <div className='flex flex-col md:flex-row items-center justify-center gap-y-8 w-[90vw] md:w-[80vw] gap-x-8'>
+      <div className='flex flex-col md:flex-row items-center justify-center gap-y-8 w-[90vw] md:w-[80vw] gap-x-8 mb-8'>
         <div className='text-center md:basis-1/2 md:text-left md:max-w-sm'>
           <h2 className='text-secondary my-4'>TESTIMONIALS</h2>
           <p className='text-lg'>Here what people have to say regarding us.</p>
         </div>
         <div>
-          <div className='border-t-8 border-t-primary rounded-md flex flex-col justify-center gap-x-4 shadow-md py-8 px-4 md:basis-1/2 max-w-lg'>
+          <div className='border-t-8 border-t-primary rounded-md flex flex-col justify-center gap-x-4 shadow-md py-8 px-4 md:basis-1/2 max-w-lg mb-4'>
             <div className='flex justify-between my-4'>
               <ImQuotesLeft />
               <ImQuotesRight />
@@ -252,12 +263,12 @@ const Home = () => {
                   ipsum est. Laboris exercitation nostrud ipsum .
                 </p>
                 <div className='flex justify-between'>
-                  <span className='flex items-center gap-x-2'>
-                    <AiOutlineMessage className='text-lg' />
+                  <span className='flex items-center gap-x-0.5 sm:gap-x-2 text-sm sm:text-base'>
+                    <AiOutlineMessage className='sm:text-lg sm:gap-x-2 text-base' />
                     Comments
                   </span>
-                  <span className='flex items-center gap-x-2'>
-                    <AiOutlineCalendar className='text-lg' />
+                  <span className='flex items-center gap-x-0.5 sm:gap-x-2 text-sm sm:text-base'>
+                    <AiOutlineCalendar className='sm:text-lg sm:gap-x-2 text-base' />
                     March 20, 2021
                   </span>
                 </div>
@@ -282,12 +293,12 @@ const Home = () => {
                   ipsum est. Laboris exercitation nostrud ipsum .
                 </p>
                 <div className='flex justify-between'>
-                  <span className='flex items-center gap-x-2'>
-                    <AiOutlineMessage className='text-lg' />
+                  <span className='flex items-center gap-x-0.5 sm:gap-x-2 text-sm sm:text-base'>
+                    <AiOutlineMessage className='sm:text-lg sm:gap-x-2 text-base' />
                     Comments
                   </span>
-                  <span className='flex items-center gap-x-2'>
-                    <AiOutlineCalendar className='text-lg' />
+                  <span className='flex items-center gap-x-0.5 sm:gap-x-2 text-sm sm:text-base'>
+                    <AiOutlineCalendar className='sm:text-lg sm:gap-x-2 text-base' />
                     March 20, 2021
                   </span>
                 </div>
@@ -300,7 +311,7 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className='hidden lg:flex flex-col shadow-md rounded-md'>
+          <div className='flex flex-col shadow-md rounded-md'>
             <div className='basis-1/2'>
               <img src={picture3} alt='' className='w-full' />
             </div>
@@ -312,12 +323,12 @@ const Home = () => {
                   ipsum est. Laboris exercitation nostrud ipsum .
                 </p>
                 <div className='flex justify-between'>
-                  <span className='flex items-center gap-x-2'>
-                    <AiOutlineMessage className='text-lg' />
+                  <span className='flex items-center gap-x-0.5 sm:gap-x-2 text-sm sm:text-base'>
+                    <AiOutlineMessage className='sm:text-lg sm:gap-x-2 text-base' />
                     Comments
                   </span>
-                  <span className='flex items-center gap-x-2'>
-                    <AiOutlineCalendar className='text-lg' />
+                  <span className='flex items-center gap-x-0.5 sm:gap-x-2 text-sm sm:text-base'>
+                    <AiOutlineCalendar className='sm:text-lg sm:gap-x-2 text-base' />
                     March 20, 2021
                   </span>
                 </div>
@@ -345,8 +356,100 @@ const Home = () => {
             placeholder='E-mail Address'
             className='max-w-[400px] w-[50vw] md:w-[30vw] border-none outline-none text-primary mr-0.5 placeholder:text-primary'
           />
-          <input type='submit' value='Submit' className='p-2 bg-secondary text-white p-3 rounded-full' />
+          <input
+            type='submit'
+            value='Submit'
+            className='p-2 bg-secondary text-white p-3 rounded-full'
+          />
         </form>
+      </div>
+      <div className='bg-primary flex justify-center items-center w-full'>
+        <div className='text-white flex flex-col md:flex-row justify-center gap-x-4 py-4 w-[90vw] font-light'>
+          <div className='md:w-1/3 flex flex-col justify-between gap-y-4'>
+            <div className='flex flex-col gap-y-2 md:mb-4'>
+              <h5 className='tetx-lg font-normal'>About Us</h5>
+              <p className='text-sm'>
+                Laboris exercitation nostrud ipsum ex culpa exercitation quis
+                ipsum est. Laboris exercitation nostrud ipsum .Laboris
+                exercitation nostrud ipsum ex culpa exercitation quis ipsum est.
+                ipsum ex culpa exercitation quis ipsum est.
+              </p>
+              <div className='flex gap-x-2'>
+                <span className='flex justify-center items-center w-6 h-6 rounded-full text-primary bg-white text-sm'>
+                  <FaFacebookF />
+                </span>
+                <span className='flex justify-center items-center w-6 h-6 rounded-full text-primary bg-white text-sm'>
+                  <FaInstagram />
+                </span>
+                <span className='flex justify-center items-center w-6 h-6 rounded-full text-primary bg-white text-sm'>
+                  <FaLinkedinIn />
+                </span>
+                <span className='flex justify-center items-center w-6 h-6 rounded-full text-primary bg-white text-sm'>
+                  <FaWhatsapp />
+                </span>
+              </div>
+            </div>
+            <span className='hidden md:block text-xs'>
+              &#169;2022. E-novate Labs. All Rights Reserved
+            </span>
+            <hr className='w-[60vw] h-2 md:hidden' />
+          </div>
+          <div className='md:w-1/3 flex flex-col md:items-center gap-y-4'>
+            <ul className='flex flex-col gap-y-2'>
+              <h1 className='text-lg font-normal'>Corporate</h1>
+              <li>About us</li>
+              <li>Contact us</li>
+              <li>Projects</li>
+              <li>Terms and Conditions</li>
+              <li>Privacy policy</li>
+            </ul>
+            <hr className='w-[60vw] h-2 md:hidden' />
+          </div>
+          <div className='md:1/3 flex flex-col justify-between gap-y-4'>
+            <div className='flex flex-col mb-4 gap-y-2'>
+              <h2 className='text-lg font-normal'>Contact Us</h2>
+              <ul className='flex flex-col gap-y-2'>
+                <li className='flex items-center gap-x-2'>
+                  <span>
+                    <BsTelephone className='text-secondary' />
+                  </span>
+                  <p>0803 353 0335</p>
+                </li>
+                <li className='flex items-center gap-x-2'>
+                  <span>
+                    <MdOutlineMailOutline className='text-secondary' />
+                  </span>
+                  <p>VillageSquareOnline@gmail.com</p>
+                </li>
+                <li className='flex items-center gap-x-2'>
+                  <span>
+                    <IoLocationOutline className='text-secondary' />
+                  </span>
+                  <p>Ivan 5, VCH Estate, Lekki, Lagos, Nigeria.</p>
+                </li>
+              </ul>
+            </div>
+            <hr className='w-[60vw] h-2 md:hidden' />
+            <div>
+              <h1 className="mb-2">Subscribe to our Newsletter</h1>
+              <form className='border-2 border-white rounded-lg flex w-fit p-1'>
+                <input
+                  type='text'
+                  name=''
+                  id=''
+                  placeholder='E-mail Address'
+                  className='border-none bg-transparent outline-none text-white mr-0.5 placeholder:text-white p-0.5'
+                />
+                <button
+                  type='submit'
+                  className='p-1 bg-secondary rounded text-white flex justify-center items-center'
+                >
+                  <AiOutlineArrowRight />
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
