41c8ad3b6ae93f1bfce168a1c6ea3409165b0622
Dewaley <adewaleademilua@gmail.com>
2022-07-02 07:08:29 +0100

    Restructured

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,21 @@
+import HomeBanner from "../components/HomeBanner";
+import Features from '../components/Features'
+import Offers from '../components/Offers'
+import CoursesOverview from '../components/CoursesOverview'
+import Testimonials from '../components/Testimonials'
+import BlogOverview from '../components/BlogOverview'
+import NewsLetter from '../components/NewsLetter'
+
 const Home = () => {
   return (
     <div className='text-white overflow-x-hidden flex flex-col justify-center items-center text-primary scroll-smooth gap-y-8'>
+      <HomeBanner/>
+      <Features/>
+      <Offers/>
+      <CoursesOverview/>
+      <Testimonials/>
+      <BlogOverview/>
+      <NewsLetter/>
     </div>
   );
 };
